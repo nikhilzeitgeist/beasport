612e5329586ef56c9b8348bea0d5fc98e15e90b3
nikhil yeole <nikhil.yeole1@gmail.com>
2012-10-01 20:07:03 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beasport::Application.config.secret_token = '74186f254b9f76260eee8e18a4d9ff045b277e8f78fa3755a5ab133ea4179cf8c7c1bf364845d14e6657fb3ae6a73239e7959b07395a6119e2dac0138ca3c0e2'
